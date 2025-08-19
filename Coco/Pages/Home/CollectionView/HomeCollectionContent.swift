1fba5e8d864181b81503164b8a78fcf984e3315b
ndan1 <lindanchristiano10@gmail.com>
2025-08-19 08:18:21 +0700

    fix: changing CollectionView to Sendable from AnyHashable

--- Coco/Pages/Home/CollectionView/HomeCollectionContent.swift
+++ Coco/Pages/Home/CollectionView/HomeCollectionContent.swift
@@ -8,18 +8,38 @@
 import Foundation
 import UIKit
 
-typealias HomeCollectionViewDataSource = UICollectionViewDiffableDataSource<HomeCollectionContent.Section, AnyHashable>
-typealias HomeCollectionViewSnapShot = NSDiffableDataSourceSnapshot<HomeCollectionContent.Section, AnyHashable>
+enum HomeCollectionItem: Hashable, Sendable {
+    case activity(HomeActivityCellDataModel)
+    
+    // Implement Hashable
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .activity(let model):
+            hasher.combine("activity")
+            hasher.combine(model)
+        }
+    }
+    
+    static func == (lhs: HomeCollectionItem, rhs: HomeCollectionItem) -> Bool {
+        switch (lhs, rhs) {
+        case (.activity(let lhsModel), .activity(let rhsModel)):
+            return lhsModel == rhsModel
+        }
+    }
+}
+
+typealias HomeCollectionViewDataSource = UICollectionViewDiffableDataSource<HomeCollectionContent.Section, HomeCollectionItem>
+typealias HomeCollectionViewSnapShot = NSDiffableDataSourceSnapshot<HomeCollectionContent.Section, HomeCollectionItem>
 
 struct HomeCollectionContent {
     let section: Section
-    let items: [AnyHashable]
+    let items: [HomeActivityCellDataModel]
     
-    enum SectionType: Hashable {
+    enum SectionType: Hashable, Sendable {
         case activity
     }
     
-    struct Section: Hashable {
+    struct Section: Hashable, Sendable {
         let type: SectionType
         let title: String?
     }
