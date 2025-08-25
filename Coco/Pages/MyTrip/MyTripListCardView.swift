f9773a18bf9131c84c35abb9f8ccf77997693042
Marcelinus Gerardo <marcelinus.gerardo@ui.ac.id>
2025-08-25 12:20:58 +0700

    style: major update mytrip card

--- Coco/Pages/MyTrip/MyTripListCardView.swift
+++ Coco/Pages/MyTrip/MyTripListCardView.swift
@@ -19,6 +19,7 @@ final class MyTripListCardView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
+        addGesture()
     }
     
     required init?(coder: NSCoder) {
@@ -32,9 +33,34 @@ final class MyTripListCardView: UIView {
         dateLabel.text = dataModel.dateText
         tripLabel.text = dataModel.title
         locationLabel.text = dataModel.location
-        totalPaxLabel.text = "\(dataModel.totalPax) person"
-        totalPriceLabel.text = "Rp \(PriceFormatting.formattedIndonesianDecimal(from: String(String(dataModel.price).dropFirst(3))))"
         imageView.loadImage(from: URL(string: dataModel.imageUrl))
+        
+        // Custom text label with NSAttributedString
+        let packageRegularFont = UIFont.jakartaSans(forTextStyle: .footnote, weight: .regular)
+        let packageBoldFont = UIFont.jakartaSans(forTextStyle: .footnote, weight: .semibold)
+        
+        let packageAttributedString = NSMutableAttributedString(
+            string: "Package: ",
+            attributes: [.font: packageRegularFont, .foregroundColor: Token.grayscale90]
+        )
+        packageAttributedString.append(NSAttributedString(
+            string: dataModel.packageType,
+            attributes: [.font: packageBoldFont, .foregroundColor: Token.grayscale90]
+        ))
+        packageTypeLabel.attributedText = packageAttributedString
+        
+        let paxBoldFont = UIFont.jakartaSans(forTextStyle: .footnote, weight: .semibold)
+        let paxRegularFont = UIFont.jakartaSans(forTextStyle: .footnote, weight: .regular)
+        
+        let paxAttributedString = NSMutableAttributedString(
+            string: "\(dataModel.totalPax)",
+            attributes: [.font: paxBoldFont, .foregroundColor: Token.grayscale90]
+        )
+        paxAttributedString.append(NSAttributedString(
+            string: " Person",
+            attributes: [.font: paxRegularFont, .foregroundColor: Token.grayscale90]
+        ))
+        totalPaxLabel.attributedText = paxAttributedString
     }
     
     private lazy var imageView: UIImageView = createImageView()
@@ -45,7 +71,7 @@ final class MyTripListCardView: UIView {
     private lazy var dateLabel: UILabel = UILabel(
         font: .jakartaSans(forTextStyle: .footnote, weight: .medium),
         textColor: Token.additionalColorsBlack,
-        numberOfLines: 2
+        numberOfLines: 1
     )
     private lazy var tripLabel: UILabel = UILabel(
         font: .jakartaSans(forTextStyle: .headline, weight: .semibold),
@@ -57,25 +83,15 @@ final class MyTripListCardView: UIView {
         textColor: Token.grayscale90,
         numberOfLines: 2
     )
+    private lazy var packageTypeLabel: UILabel = UILabel(
+        font: .jakartaSans(forTextStyle: .footnote, weight: .regular),
+        textColor: Token.grayscale90,
+        numberOfLines: 1
+    )
     private lazy var totalPaxLabel: UILabel = UILabel(
         font: .jakartaSans(forTextStyle: .footnote, weight: .semibold),
         textColor: Token.grayscale90,
-        numberOfLines: 2
-    )
-    private lazy var totalPriceLabel: UILabel = UILabel(
-        font: .jakartaSans(forTextStyle: .footnote, weight: .semibold),
-        textColor: Token.additionalColorsBlack,
-        numberOfLines: 2
-    )
-    private lazy var detailButtonContainer: CocoButtonHostingController = CocoButtonHostingController(
-        action: { [weak self] in
-            guard let self else { return }
-            delegate?.notifyTripListCardDidTap(at: index)
-        },
-        text: "Detail",
-        style: .normal,
-        type: .primary,
-        isStretch: true
+        numberOfLines: 1
     )
     
     private var index: Int = 0
@@ -83,157 +99,116 @@ final class MyTripListCardView: UIView {
 
 private extension MyTripListCardView {
     func setupView() {
-        let dateContentView: UIView = UIView()
-        dateContentView.addSubviews([
-            statusLabelView.view,
-            dateLabel
-        ])
+        // Top container for Status and Date
+        let topContainer = UIView()
+        topContainer.addSubviews([statusLabelView.view, dateLabel])
         
-        statusLabelView.view
-            .layout {
-                $0.leading(to: dateContentView.leadingAnchor)
-                    .top(to: dateContentView.topAnchor)
-                    .bottom(to: dateContentView.bottomAnchor)
-            }
-        
-        dateLabel.layout {
-            $0.leading(to: statusLabelView.view.trailingAnchor, relation: .greaterThanOrEqual, constant: 4.0)
-                .top(to: dateContentView.topAnchor)
-                .bottom(to: dateContentView.bottomAnchor)
-                .trailing(to: dateContentView.trailingAnchor)
+        statusLabelView.view.layout {
+            $0.leading(to: topContainer.leadingAnchor)
+                .top(to: topContainer.topAnchor)
+                .bottom(to: topContainer.bottomAnchor)
         }
         
-        let locationContentView: UIView = UIView()
-        let locationIconImageView: UIImageView = UIImageView(image: CocoIcon.icPinPointBlue.getImageWithTintColor(Token.additionalColorsBlack))
-        locationIconImageView.contentMode = .scaleAspectFill
+        dateLabel.layout {
+            $0.trailing(to: topContainer.trailingAnchor)
+                .centerY(to: statusLabelView.view.centerYAnchor)
+        }
+        
+        // Location content view
+        let locationContentView = UIView()
+        let locationIconImageView = UIImageView(image: CocoIcon.icPinPointBlue.getImageWithTintColor(Token.grayscale90))
+        locationIconImageView.contentMode = .scaleAspectFit
         locationIconImageView.layout {
             $0.size(12)
         }
-        locationContentView.addSubviews([
-            locationIconImageView,
-            locationLabel
-        ])
+        locationContentView.addSubviews([locationIconImageView, locationLabel])
         
-        locationIconImageView
-            .layout {
-                $0.leading(to: locationContentView.leadingAnchor)
-                    .top(to: locationContentView.topAnchor)
-                    .bottom(to: locationContentView.bottomAnchor)
-            }
+        locationIconImageView.layout {
+            $0.leading(to: locationContentView.leadingAnchor)
+                .top(to: locationContentView.topAnchor)
+                .bottom(to: locationContentView.bottomAnchor)
+        }
         
         locationLabel.layout {
             $0.leading(to: locationIconImageView.trailingAnchor, constant: 4.0)
-                .centerY(to: locationContentView.centerYAnchor)
+                .centerY(to: locationIconImageView.centerYAnchor)
                 .trailing(to: locationContentView.trailingAnchor)
         }
         
-        let paxContentView: UIView = UIView()
-        paxContentView.addSubviews([
-            totalPaxLabel,
-            totalPriceLabel
-        ])
-        
-        totalPaxLabel
-            .layout {
-                $0.leading(to: paxContentView.leadingAnchor)
-                    .top(to: paxContentView.topAnchor)
-                    .bottom(to: paxContentView.bottomAnchor)
-            }
-        
-        totalPriceLabel.layout {
-            $0.leading(to: totalPaxLabel.trailingAnchor, relation: .greaterThanOrEqual, constant: 4.0)
-                .top(to: paxContentView.topAnchor)
-                .bottom(to: paxContentView.bottomAnchor)
-                .trailing(to: paxContentView.trailingAnchor)
-        }
-        
-        let leftSideContentView: UIView = UIView()
-        leftSideContentView.addSubviews([
-            dateContentView,
+        // Right side vertical stack
+        let rightSideStackView = UIStackView(arrangedSubviews: [
             tripLabel,
-            locationContentView,
-            paxContentView
+            locationContentView
         ])
-        dateContentView
-            .layout {
-                $0.leading(to: leftSideContentView.leadingAnchor)
-                    .top(to: leftSideContentView.topAnchor)
-                    .trailing(to: leftSideContentView.trailingAnchor)
-            }
+        rightSideStackView.axis = .vertical
+        rightSideStackView.spacing = 4.0
+        rightSideStackView.alignment = .leading
         
-        tripLabel.layout {
-            $0.top(to: dateContentView.bottomAnchor, constant: 6.0)
-                .leading(to: leftSideContentView.leadingAnchor)
-                .trailing(to: leftSideContentView.trailingAnchor)
-        }
+        // Main content container
+        let mainContentContainer = UIView()
+        mainContentContainer.addSubviews([imageView, rightSideStackView])
         
-        locationContentView.layout {
-            $0.top(to: tripLabel.bottomAnchor, constant: 4.0)
-                .leading(to: leftSideContentView.leadingAnchor)
-                .trailing(to: leftSideContentView.trailingAnchor)
-        }
-        
-        paxContentView.layout {
-            $0.top(to: locationContentView.bottomAnchor, constant: 8.0)
-                .leading(to: leftSideContentView.leadingAnchor)
-                .trailing(to: leftSideContentView.trailingAnchor)
-                .bottom(to: leftSideContentView.bottomAnchor)
-        }
-        
-        let headerView: UIView = UIView()
-        headerView.addSubviews([
-            imageView,
-            leftSideContentView
-        ])
         imageView.layout {
-            $0.leading(to: headerView.leadingAnchor)
-                .top(to: headerView.topAnchor)
-                .bottom(to: headerView.bottomAnchor, relation: .lessThanOrEqual)
+            $0.leading(to: mainContentContainer.leadingAnchor)
+                .top(to: mainContentContainer.topAnchor)
+                .bottom(to: mainContentContainer.bottomAnchor, relation: .lessThanOrEqual)
         }
         
-        leftSideContentView.layout {
-            $0.leading(to: imageView.trailingAnchor, constant: 12.0)
-                .top(to: headerView.topAnchor)
-                .trailing(to: headerView.trailingAnchor)
-                .bottom(to: headerView.bottomAnchor)
+        rightSideStackView.layout {
+            $0.leading(to: imageView.trailingAnchor, constant: 16.0)
+                .top(to: mainContentContainer.topAnchor)
+                .trailing(to: mainContentContainer.trailingAnchor)
+                .bottom(to: mainContentContainer.bottomAnchor, relation: .lessThanOrEqual)
         }
         
-        let contentView: UIView = UIView()
-        contentView.addSubviews([
-            headerView,
-            detailButtonContainer.view
+        // Bottom info stack
+        let bottomInfoStackView = UIStackView(arrangedSubviews: [
+            packageTypeLabel,
+            totalPaxLabel
         ])
-        headerView.layout {
-            $0.leading(to: contentView.leadingAnchor)
-                .top(to: contentView.topAnchor)
-                .trailing(to: contentView.trailingAnchor)
-        }
+        bottomInfoStackView.axis = .vertical
+        bottomInfoStackView.spacing = 4.0
+        bottomInfoStackView.alignment = .leading
         
-        detailButtonContainer.view.layout {
-            $0.top(to: headerView.bottomAnchor, constant: 12.0)
-                .leading(to: contentView.leadingAnchor)
-                .trailing(to: contentView.trailingAnchor)
-                .bottom(to: contentView.bottomAnchor)
-        }
+        // Main vertical stack
+        let mainStackView = UIStackView(arrangedSubviews: [
+            topContainer,
+            mainContentContainer,
+            bottomInfoStackView
+        ])
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 12.0
         
-        addSubviewAndLayout(contentView, insets: UIEdgeInsets(edges: 12.0))
+        addSubviewAndLayout(mainStackView, insets: UIEdgeInsets(edges: 16.0))
         
         backgroundColor = Token.additionalColorsWhite
         layer.cornerRadius = 16.0
-        layer.borderWidth = 1.0
-        layer.borderColor = Token.additionalColorsLine.cgColor
+        layer.borderWidth = 0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 4.0
+        clipsToBounds = false
     }
     
     func createImageView() -> UIImageView {
-        let imageView: UIImageView = UIImageView()
+        let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 14.0
         imageView.layout {
-            $0.width(89)
-                .height(120)
+            $0.width(100)
+                .height(100)
         }
-        
         return imageView
+    }
+    
+    func addGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cardTapped))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func cardTapped() {
+        delegate?.notifyTripListCardDidTap(at: index)
     }
 }
